338ada9f1789741b110366b6cbcb53a3a8a6d521
Amii Jiang <amii@MacBook-Pro.local>
2022-11-19 09:57:36 -0500

    Up-to-date

---
+++ controllers/breads_controller.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const breads = express.Router()
+const Bread = require('../models/bread.js')
+
+// INDEX
+breads.get('/', (req, res) => {
+    res.render('Index',
+      {
+        breads: Bread
+      }
+    )
+})
+
+
+// SHOW
+// /breads/2
+breads.get('/:arrayIndex', (req, res) => {
+    res.send(Bread[req.params.arrayIndex])
+  })  
+
+module.exports = breads
---
+++ server.js
@@ -0,0 +1,27 @@
+// DEPENDENCIES
+const express = require('express')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+  
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+})
