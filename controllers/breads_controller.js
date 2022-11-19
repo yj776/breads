a859099ad11049904aa88ec4f5db8624b8be7bdb
Amii Jiang <amii@MacBook-Pro.local>
2022-11-19 10:39:50 -0500

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -13,9 +13,22 @@ breads.get('/', (req, res) => {
 
 
 // SHOW
-// /breads/2
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })  
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
+})
+
+
+// // /breads/2
+// breads.get('/:arrayIndex', (req, res) => {
+//     res.render('Show', {
+//       bread: Bread[req.params.arrayIndex]
+//     })
+//   })  
 
 module.exports = breads
